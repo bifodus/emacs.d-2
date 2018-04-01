179fb49baeb4fc8e6f3f9d1e5d86258f39b0f4f0
Eric Schneider <bifodus@gmail.com>
2018-04-01 09:56:09 -0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,19 @@
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(winner-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-set-key (kbd "M-o") 'other-window)
+(windmove-default-keybindings)
