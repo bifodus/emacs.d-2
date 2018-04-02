a2a89bfd8edf58d4b50f7bbbe0e75bb6f5791f33
Eric Schneider <bifodus@gmail.com>
2018-04-01 21:35:17 -0700

    Add stuff

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")))
@@ -8,6 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
+ '(package-selected-packages (quote (magit avy ace-jump-mode org)))
  '(winner-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -17,3 +24,19 @@
  )
 (global-set-key (kbd "M-o") 'other-window)
 (windmove-default-keybindings)
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
+(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(setq org-log-done t)
+
+
+
+
+
+
